d3dd9d04776d8396500c414960e003caaf43fcd1
Nguyen Huu Duc <diemannschaft2003@gmail.com>
2024-10-28 21:30:22 +0700

    Update star image

--- app/src/main/java/com/example/emailinbox/MainActivity.kt
+++ app/src/main/java/com/example/emailinbox/MainActivity.kt
@@ -69,6 +69,41 @@ class MainActivity : AppCompatActivity() {
                 "Announcing the all-new Creator, build out of this world content",
                 "10:14 AM",
                 colors.random()
+            ),
+            EmailItem(
+                "Edurila.com",
+                "$19 Only (First 10 spots) - Bestselling in online course",
+                "Are you looking to Learn Web Designing on our site?",
+                "12:34 PM",
+                colors.random()
+            ),
+            EmailItem(
+                "Chris Abad",
+                "Help make Campaign Monitor better",
+                "Let us know your thoughts! No Images needed!",
+                "11:22 AM",
+                colors.random()
+            ),
+            EmailItem(
+                "Tuto.com",
+                "8h de formation gratuite et les nouveau",
+                "Photoshop, SEO, Blender, CSS, WordPress, HTML...",
+                "11:04 AM",
+                colors.random()
+            ),
+            EmailItem(
+                "support",
+                "Société Ovh : suivi de vos services - hp lorem ipsum...",
+                "SAS OVH - http://www.ovh.com 2 rue lorem ipsum",
+                "10:26 AM",
+                colors.random()
+            ),
+            EmailItem(
+                "Matt from Ionic",
+                "The New Ionic Creator Is Here!",
+                "Announcing the all-new Creator, build out of this world content",
+                "10:14 AM",
+                colors.random()
             )
         )
     }
